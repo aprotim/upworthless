7a517c2b37d4e430aa0e400574101877a6918e2e
aprotim <github.aprotim@xoxy.net>
2013-10-18 15:54:16 -0700

    Installation instructions in the comments

--- upworthless.user.js
+++ upworthless.user.js
@@ -2,4 +2,7 @@
 // @match http://*.upworthy.com/*
 // ==/UserScript==
 
+// (Haphazardly) tested in Chrome.
+// To install: save the raw .js file, go to about:extensions, and drag the saved js file to the window.
+
 location.href = document.evaluate("//a[text()='ORIGINAL']/@href", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent
