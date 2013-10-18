193886fa6a6d44e29f797041329a9fba08c252bd
aprotim <github.aprotim@xoxy.net>
2013-10-18 16:06:02 -0700

    Simplify. If the link starts with "ORIGINAL", call it good.

--- upworthless.user.js
+++ upworthless.user.js
@@ -5,4 +5,4 @@
 // (Haphazardly) tested in Chrome.
 // To install: save the raw .js file, go to about:extensions, and drag the saved js file to the window.
 
-location.href = document.evaluate("//a[text()='ORIGINAL' or text()='ORIGINAL:']/@href", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent
+location.href = document.evaluate("//a[starts-with(text(), 'ORIGINAL')]/@href", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent
